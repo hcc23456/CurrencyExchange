0fbc9a6e15925db06fdddfceaede6032f41b1b8f
Henry Chan <hcc23456@gmail.com>
2019-06-08 01:30:43 -0700

    change config for aws hosting

--- CurrencyExchange/Program.cs
+++ CurrencyExchange/Program.cs
@@ -18,8 +18,21 @@ namespace CurrencyExchange
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
+            /*WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .Build();
+                .Build();*/
+				
+			//configuring for deployment onto aws/any external url
+			//https://stackoverflow.com/questions/37974451/how-to-run-a-net-core-mvc-site-on-aws-linux-instance - setup port forwarding to 5000 from 80 if needed
+			var host = new WebHostBuilder()
+				.UseKestrel()
+				.UseContentRoot(Directory.GetCurrentDirectory())
+				//.UseUrls("http://localhost:5000", "http://xxxx:5000", "http://192.168.1.2:5000") //specified urls only
+				.UseUrls("http://*:80") //essential whether using kestrel or iis, accepts anything from the specified port
+				.UseIISIntegration()
+				.UseStartup<Startup>()
+				.Build();
+				
+			host.Run();
     }
 }
